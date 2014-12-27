17311bb7f63b425721644ae2d08c42a737746d2d
Kirill Ignatiev <kirill.ignatiev@gmail.com>
2014-12-26 21:48:25 -0500

    Shorter lines, remove fixme.

--- test.hs
+++ test.hs
@@ -3,10 +3,11 @@ module Main where
 main :: IO ()
 main :: Int -> Integer -> Ratio Integer -> Double -> Bool -> IO ()
 main = elem . notElem . union . intersect . msum
-main = [1..] ++ [1..2] ++ [1,2..3] -< a >- b <*> c >> d << e
-main = \x -> f >>= g =<< h >>> i <<< j *** k ++ l +++ m ||| n
+main = [1..] ++ [1..2] ++ [1,2..3]
+main = x -< a >- b <*> c >> d << e
+main = \x -> f >>= g =<< h >>> i
+main = x <<< j *** k ++ l +++ m ||| n
 main = 1 >= 2 && 2 <= 1 || 1 == 2 || 1 /= 2
--- FIXME All of these should be greek letters
 alpha = 1
 beta = 2
 alpha_beta = alpha_beta
